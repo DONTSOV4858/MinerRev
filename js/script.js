bd9e52ab5d53c925a617600d98cfdff60047e7e8
Dons <dontsov4858@gmail.com>
2026-04-02 17:45:56 +0300

    Enhance responsiveness and navigation; update media queries in CSS, add smooth scrolling, and improve menu toggle functionality

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ const menuBtn = document.getElementById("nav_button");
 const menu = document.getElementById("menu");
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
+  menuBtn.classList.toggle("hide-button");
 });
 const close = document.getElementById("closemenu");
 close.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
+  menuBtn.classList.toggle("hide-button");
 });
 
 const menuLinks = document.querySelectorAll(".header__link a");
@@ -13,5 +15,6 @@ const menuLinks = document.querySelectorAll(".header__link a");
 menuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     menu.classList.remove("show-menu");
+    menuBtn.classList.toggle("hide-button");
   });
 });
